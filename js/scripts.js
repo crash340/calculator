c83b41a1621df90e1b756c0a24f790c5e1e1321f
mohamed hashi <kidnoodle12@gmail.com>
2019-07-11 09:48:52 +0300

    added js with scripts.js and css folder and index.html

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var add = function(number1,number2){
+return number1 + number2
+};
+
+var number1 = parseInt(prompt("Enter a NUMBER:"));
+var number2 = parseInt(prompt("Enter a another NUMBER:"))
+alert(add(number1,number2));
